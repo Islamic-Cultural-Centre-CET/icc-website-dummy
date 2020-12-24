447f8cc9d83b5421fc8688ea415e21491e290a7d
taqnar <muzammilthottathil.official@gmail.com>
2020-12-24 16:50:41 +0530

    completed homepage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector('#hamburger')
 const hamburgerClose = document.querySelector('#hamburger-close')
 const sideNav = document.querySelector('.side-nav')
+const expolreMore = document.querySelector('.explore-more')
+const readMoreButton = document.querySelector('#read-more')
+const readMoreContent = document.querySelector('#more-content')
+const navList = document.querySelector('#nav-list').children
 
 hamburger.addEventListener('click', () => {
     sideNav.classList.remove('hide-side-nav')
@@ -8,4 +12,25 @@ hamburger.addEventListener('click', () => {
 
 hamburgerClose.addEventListener('click', () => {
     sideNav.classList.add('hide-side-nav')
+})
+
+Object.values(navList).forEach((item) => {
+    item.addEventListener('click', () => {
+        sideNav.classList.add('hide-side-nav')
+    })
+})
+
+window.onscroll = () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollTop
+    expolreMore.style.display = scroll > 100 ? 'none' : 'block'
+}
+
+readMoreButton.addEventListener('click', () => {
+    if (readMoreButton.textContent === 'READ MORE') {
+        readMoreButton.textContent = 'READ LESS'
+        readMoreContent.style.display = 'inline'
+    } else {
+        readMoreButton.textContent = 'READ MORE'
+        readMoreContent.style.display = 'none'
+    }
 })
